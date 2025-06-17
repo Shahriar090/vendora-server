fe753f468f6ffaa04eb213708f712ba4c71445ae
Shahriar <shahriarh655@gmail.com>
2025-06-17 18:36:52 +0600

    Add asyncHandler and sendResponse utilities for cleaner error handling and API responses.

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+
+const asyncHandler = (func: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(func(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export default asyncHandler;
