8615f8527285afc3ea0fbc76c57d90266266b36a
Shahriar <shahriarh655@gmail.com>
2025-06-19 23:02:10 +0600

    Updated app.ts. Fixing cors related issues by adding origin.

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://vendora-e-commerce.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
