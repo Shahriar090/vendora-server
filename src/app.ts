0a4614fbdc3a95afbc99f5a75baf6b68381f7c56
Shahriar <shahriarh655@gmail.com>
2025-06-19 14:29:13 +0600

    Updated app.ts. Fixed cross-origin CORS issues by adding the client-side origin.

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
+    origin: ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
